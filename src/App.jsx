83d0640a38f75d74c03c1c5490ea238dde359f54
Sachin Malik <147371547+sachin-singh-malik@users.noreply.github.com>
2024-09-30 20:17:15 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import ExpenseForm from "./Components/ExpenseForm";
+import ExpenseTable from "./Components/ExpenseTable";
+import expenseData from "./expenseData";
+import { useLocalStorage } from "./Hooks/useLocalStorage";
+
+function App() {
+  const [expense, setExpense] = useLocalStorage("expense", {
+    title: "",
+    category: "",
+    amount: "",
+  });
+  const [expenses, setExpenses] = useLocalStorage("expenses", expenseData);
+  const [editingRowId, setEditingRowId] = useLocalStorage("editingRowId", "");
+
+  return (
+    <main>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm
+          setExpenses={setExpenses}
+          expense={expense}
+          setExpense={setExpense}
+          editingRowId={editingRowId}
+          setEditingRowId={setEditingRowId}
+        />
+        <ExpenseTable
+          expenses={expenses}
+          setExpense={setExpense}
+          setExpenses={setExpenses}
+          setEditingRowId={setEditingRowId}
+        />
+      </div>
+    </main>
+  );
+}
+
+export default App;
